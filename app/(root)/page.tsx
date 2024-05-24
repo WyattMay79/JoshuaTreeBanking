772f709709448aba39040b0a0a1968237638c27f
Wyatt May <wyattmay79@gmail.com>
2024-05-24 12:16:02 -0600

    Implemented the right home bar that displays account info and currently linked bank cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,40 @@
 import { HeaderBox } from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { Lasso } from "lucide-react";
 import React from "react";
 
 const Home = () => {
-    const loggedIn = { firstName: 'Wyatt' };
+    const loggedIn = { firstName: 'William', lastName: 'Shakespeare', email: 'contact@shakespeare.com' };
+
     return (
         <section className="home" >
             <div className="home-content">
                 <header className="home-header">
-                    <HeaderBox 
-                        type="greeting" 
+                    <HeaderBox
+                        type="greeting"
                         title="Welcome"
                         user={loggedIn?.firstName || "Guest"}
-                        subtext ="Access and manage your account and transactions efficiently."
+                        subtext="Access and manage your account and transactions efficiently."
                     />
 
-                    <TotalBalanceBox 
+                    <TotalBalanceBox
                         accounts={[]}
                         totalBanks={1}
                         totalCurrentBalance={1250.35}
                     />
                 </header>
+
+                RECENT TRANSACTIONS
+
             </div>
+
+            <RightSideBar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 123.45 }, { currentBalance: 1250.00 }]}
+            />
+
         </section>
     )
 }
